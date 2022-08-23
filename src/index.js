fd24f1b1edf8121817becdbb50451fd94d3904a3
Dhuruv Singh <84073153+DhuruvSingh@users.noreply.github.com>
2022-08-24 01:36:26 +0530

    Add Array, some style, javascript code

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
-document.getElementById('heading').innerHTML = 'abhi sahi hai';
+// import _ from 'lodash';
+import './styles.css';
+
+const scorecard = [
+  {
+    id: 0,
+    name: 'test1',
+    score: '5000',
+  },
+  {
+    id: 1,
+    name: 'test2',
+    score: '5000',
+  },
+  {
+    id: 2,
+    name: 'test3',
+    score: '5000',
+  },
+  {
+    id: 3,
+    name: 'test4',
+    score: '5000',
+  },
+  {
+    id: 4,
+    name: 'test5',
+    score: '5000',
+  },
+];
+
+document.getElementById('scoreList').innerHTML = scorecard.map((items) => `
+<p class="eachScore">${items.name}: ${items.score}</p>`).join(' ');
